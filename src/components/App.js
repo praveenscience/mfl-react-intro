678b15f0952d582d00733db9c2ccea146b6a508a
Praveen Kumar <praveenscience@gmail.com>
2020-12-27 12:46:39 +0000

    Reorganised all the files.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Everyone</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          veniam ullam eveniet aspernatur expedita suscipit, itaque sint, alias
+          totam, dignissimos omnis necessitatibus nam sed laboriosam praesentium
+          iure. Ipsa, numquam in.
+        </p>
+      </div>
+    );
+  }
+}
