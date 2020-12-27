8d425290a9867462b43bae65a1208c53c50dd67d
Praveen Kumar <praveenscience@gmail.com>
2020-12-27 13:06:33 +0000

    Created Header Component and styles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Everyone</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          veniam ullam eveniet aspernatur expedita suscipit, itaque sint, alias
-          totam, dignissimos omnis necessitatibus nam sed laboriosam praesentium
-          iure. Ipsa, numquam in.
+        <Header dark={true}>React App</Header>
+        <p className="mt-3 text-center">
+          Hey, we just built an awesome React App!
         </p>
       </div>
     );
